6e43ecbee13c2633530b11feaf09915c767ad9c2
Shaishav Gupta <shaishav@nected.ai>
2024-05-22 19:13:06 +0530

    fix: uncommented test cases code

--- events/tests.py
+++ events/tests.py
@@ -7,28 +7,28 @@ from django.contrib.auth import get_user_model
 from users.models import User
 
 
-# class TestEvent(TestCase):
-#     def setUp(self):
-#         authUser = get_user_model().objects.create(
-#             username='testuser',
-#             password='testuser',
-#         )
-#         self.event = Event.objects.create(
-#             name='Test Event',
-#             created_by=authUser,
-#             total_tickets = 100,
-#             available_tickets = 100,
-#             lat = 10.1,
-#             lng = 10.1,
-#             price = 100.1,
-#             date = '2021-08-01'
-#             )
+class TestEvent(TestCase):
+    def setUp(self):
+        authUser = get_user_model().objects.create(
+            username='testuser',
+            password='testuser',
+        )
+        self.event = Event.objects.create(
+            name='Test Event',
+            created_by=authUser,
+            total_tickets = 100,
+            available_tickets = 100,
+            lat = 10.1,
+            lng = 10.1,
+            price = 100.1,
+            date = '2021-08-01'
+            )
     
-#     def test_event_creation(self):
-#         self.assertEqual(cache.get(f'event_{self.event.id}'), self.event.available_tickets)
+    def test_event_creation(self):
+        self.assertEqual(cache.get(f'event_{self.event.id}'), self.event.available_tickets)
     
-#     def test_event_tickets(self):
-#         self.assertLessEqual(self.event.total_tickets, self.event.available_tickets)
+    def test_event_tickets(self):
+        self.assertLessEqual(self.event.total_tickets, self.event.available_tickets)
 
 
 class TestBooking(TestCase):
