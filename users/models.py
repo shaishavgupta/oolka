0ad65766285959d23179739b76d815f8aa2678de
Shaishav Gupta <shaishav@nected.ai>
2024-05-31 11:35:54 +0530

    added user signup api

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=30, blank=False, null=False)
     email = models.EmailField(blank=False, null=False, db_index=True, unique=True)
+    password = models.CharField(max_length=30, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True, blank=False, null=False)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+class SignupView(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        name = request.data.get("name")
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        if not name:
+            return Response({"message": "name is required"}, status=status.HTTP_400_BAD_REQUEST)
+        if not email:
+            return Response({"message": "email is required"}, status=status.HTTP_400_BAD_REQUEST)
+        if not password:
+            return Response({"message": "password is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        user, created = User.objects.get_or_create(email=email, defaults={"name": name, "password": hash(password)})
+
+        if created:
+            return Response({"message": "user created", "data":self.serializer_class(user).data}, status=status.HTTP_201_CREATED)
+        
+        return Response({"message": "user already exists", "data": self.serializer_class(user).data}, status=status.HTTP_200_OK)
